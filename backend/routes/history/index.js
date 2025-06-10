7a0c8215cdfe56a308e62be07c6d6e00e51624ec
aidanbradley <bradl154@purdue.edu>
2025-06-10 17:34:52 -0400

    PRE-BOOK HiSTORY CHANGING STUFF

--- backend/routes/history/index.js
+++ backend/routes/history/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../../models/User");
 const axios = require("axios")
 const KEY = process.env.GOOGLE_BOOKS_API_KEY;
+const db = require("../../database")
 
 // Get the frontend API request for the user profile history section.
 router.get("/user-search", async (req, res) => {
@@ -51,5 +52,36 @@ router.get("/user-search", async (req, res) => {
     res.status(500).json({ error: "Failed to search books" });
   }
 });
+
+
+// Route to save a book to the user's history list after adding it
+router.post("/user-books", async(req, res) => {
+  const { user_id, book_id, title, author, cover_image} = req.body;
+
+  // Validation
+  if (!user_id || !book_id || !title) {
+    return res.status(400).json({ message: "Missing field "})
+  }
+
+  try {
+    const query = `
+      INSERT INTO user_books (user_id, book_id, title, author, cover_image)
+      VALUES (?, ?, ?, ?, ?)
+    `;
+
+    await db.execute(query, [user_id, book_id, title, author, cover_image])
+    res.status(201).json({ message: "Book saved success!"})
+
+  } catch (error) {
+    if (error.code == `ER_DUP_ENTRY`) {
+      return res.status(409).json({message: "Book already saved"})
+    }
+
+    console.error("Error saving book:", error);
+    res.status(500).json({message: "Failed to save deh book!"})
+  }
+})
+
+
 
 module.exports = router;
